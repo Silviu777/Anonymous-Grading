58d2fd59b11577238bfbbcddce85e8526b4aa86c
Silviu Obreajan <obreajansilviu@yahoo.com>
2021-11-27 12:05:19 +0200

    "updates"

---
+++ web/router.mjs
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const router = express.router();
+
+// ROUTES (/students; /students/id; /project; /project/projectId)
---
+++ web/service.mjs
@@ -0,0 +1,7 @@
+import Sequelize from 'sequelize';
+
+// import { CLASSES HERE } from './classes_locations';
+
+// CRUD operations (create, reade, update, delete)
+
+// async function getStudent(request, response) {}
