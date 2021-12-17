37a8f871c64944f2bde3cb1cf61a9cf6d8f2e342
Silviu Obreajan <obreajansilviu@yahoo.com>
2021-12-17 18:17:41 +0200

    code cleanup

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
 import express, { json } from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { rootCertificates } from "tls";
 import router from './router.js';
 import { init } from './repository.js'
-
-// const server = express();
-
-// server.use(express.urlencoded({ extended: true }));
-// server.use(express.json());
-// server.use(cors());
-// server.use("/api", router);
-
-
-// server.get('/api', (req, res) => res.send('WORKS'));
-// server.listen(process.env.PORT || 8080, () => console.log("App Server running..."));
-
-// export { server, router };
 
 express()
     .use(json())
